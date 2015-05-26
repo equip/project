66b8b6ecb25e0dbdfd1ef5d9eafa5d22f97eeb47
Woody Gilk <woody.gilk@gmail.com>
2015-05-26 12:42:25 -0500

    Simplify bootstrapping
    Depends on sparkphp/spark#2

--- web/index.php
+++ web/index.php
@@ -1,15 +1,13 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+define('APP_PATH', realpath(__DIR__ . '/..') . DIRECTORY_SEPARATOR);
 
-define('APP_PATH',realpath(__DIR__ . '/..') . DIRECTORY_SEPARATOR);
+require APP_PATH . 'vendor/autoload.php';
 
-$app = new Spark\Application();
+$app = Spark\Application::boot();
 
-$app->addRoutes(function(\Spark\Router $r) {
-
-    $r->get('/shifts', '\Spark\Project\Domain\Shift\GetList');
-
+$app->addRoutes(function(Spark\Router $r) {
+    $r->get('/shifts', 'Spark\Project\Domain\Shift\GetList');
 });
 
 $app->run();
