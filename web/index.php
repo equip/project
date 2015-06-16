b3fa07852437304a5404a493e8a961cd485bb3ad
Daniel Olfelt <dolfelt@gmail.com>
2015-06-16 17:07:28 -0500

    Add middleware queue for new Relay library.

--- web/index.php
+++ web/index.php
@@ -6,6 +6,13 @@ require APP_PATH . 'vendor/autoload.php';
 
 $app = Spark\Application::boot();
 
+$app->setMiddleware([
+    'Relay\Middleware\ResponseSender',
+    'Spark\Handler\ExceptionHandler',
+    'Spark\Handler\RouteHandler',
+    'Spark\Handler\ActionHandler',
+]);
+
 $app->addRoutes(function(Spark\Router $r) {
     $r->get('/shifts', 'Spark\Project\Domain\Shift\GetList');
 });
