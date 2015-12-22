1aa6908906aeb8b5407ac311550dd6eec0b54d41
Woody Gilk <woody.gilk@gmail.com>
2015-12-21 19:52:05 -0600

    Remove even more unncessary slashes

--- web/index.php
+++ web/index.php
@@ -4,27 +4,27 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // Configure the dependency injection container
-$injector = new \Auryn\Injector;
-$configuration = new \Spark\Configuration\DefaultConfigurationSet;
+$injector = new Auryn\Injector;
+$configuration = new Spark\Configuration\DefaultConfigurationSet;
 $configuration->apply($injector);
 
 // Configure middleware
 $injector->alias(
-    '\Spark\Middleware\Collection',
-    '\Spark\Middleware\DefaultCollection'
+    'Spark\Middleware\Collection',
+    'Spark\Middleware\DefaultCollection'
 );
 
 // Configure the router
 $injector->prepare(
-    '\Spark\Router',
-    function(\Spark\Router $router) {
+    'Spark\Router',
+    function (Spark\Router $router) {
         $router->get('/hello[/{name}]', 'Spark\Project\Domain\Hello');
         $router->post('/hello[/{name}]', 'Spark\Project\Domain\Hello');
     }
 );
 
 // Bootstrap the application
-$dispatcher = $injector->make('\Relay\Relay');
+$dispatcher = $injector->make('Relay\Relay');
 $dispatcher(
     $injector->make('Psr\Http\Message\ServerRequestInterface'),
     $injector->make('Psr\Http\Message\ResponseInterface')
